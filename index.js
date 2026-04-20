196e16abc5c9c2aad28298e71d4cccf235807fc7
Rob Bickley <robert.john.bickley@gmail.com>
2026-04-20 14:19:38 -0400

    Add script to HTML file. Add JS file

---
+++ index.js
@@ -0,0 +1,26 @@
+let code1;
+code1 = 7 + 3;
+console.log(code1);
+
+let code2;
+code2 = 90 / 2 - 5;
+console.log(code2);
+
+let code3;
+code3 = 139 % 50;
+console.log(code3);
+
+const message = "The vault has been secured. the combination is:";
+console.log(message);
+
+const codea = code1 + "-" + code2 + "-" + code3;
+console.log(codea);
+
+const codeb = `${code1}-${code2}-${code3}`;
+console.log(codeb);
+
+const answera = typeof codea;
+console.log(answera);
+
+const answerb = typeof codeb;
+console.log(answerb);
